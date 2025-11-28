ce454bfec1ca3cca0ff253e1f5a4708bf5059683
Nanthini Palani <48705904+Nanthini7701@users.noreply.github.com>
2025-11-28 19:33:33 +0530

    Add STATIC_ROOT to Django settings

--- giftshop/settings.py
+++ giftshop/settings.py
@@ -123,6 +123,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'shop' / 'static' ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
